4dec5bdc5a00cdc914e9f370d8c3b41b8bd6f841
yogskr <96821189+yogskr@users.noreply.github.com>
2023-11-25 21:39:57 +0700

    added 6-squareNums.js

---
+++ 6-squareNums.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Write a function, squareNums(), that takes in an array of numbers and, using .map(), returns an array with the square of each of the elements of that array.
+
+const numbers = [2, 7, 9, 171, 52, 33, 14];
+
+const toSquare = (num) => num * num;
+
+const squareNums = (nums) => {
+  return nums.map(toSquare);
+};
+
+console.log(squareNums(numbers));
